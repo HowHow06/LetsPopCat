7d2123a87213066deda07ac34ce1318f42155d6d
Howard <howard_bb@hotmail.com>
2021-08-13 19:45:01 +0800

    Update AutoCatPopper.js

--- AutoCatPopper.js
+++ AutoCatPopper.js
@@ -6,4 +6,8 @@ const pop = () => {
 
 const autoPop = setInterval(pop, 100);
 
+
+//--Copy and paste the code above to console to use this code, but make sure you understand the code
+//--把上面的代码复制贴上到Console里面就行了，当然请你先试着理解代码
+
 clearInterval(autoPop); //only use this code when you wanna stop the popping
