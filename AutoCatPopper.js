0e2f9fbc84d1d38fcfcfef8a64cba1bb73013e23
Howard <howard_bb@hotmail.com>
2021-08-13 19:40:35 +0800

    Add auto cat popper js code
    initial upload

---
+++ AutoCatPopper.js
@@ -0,0 +1,7 @@
+const pointerDown = new PointerEvent("pointerdown");
+const pop = () => {
+  document.dispatchEvent(pointerDown);
+  app.__vue__.sequential_max_pops = 0; //reset the sequential count every single time
+};
+
+const autoPop = setInterval(pop, 100);
